f24d56e108aa57073db3600b86e480c481e84eca
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2024-01-25 09:14:33 -0300

    update: menu

--- resources/views/includes/menu.blade.php
+++ resources/views/includes/menu.blade.php
@@ -1,8 +1,8 @@
 <nav class="flex">
     <div class="flex flex-row justify-between items-center px-12 gap-x-4 
-        h-16 text-slate-900 dark:text-zinc-50 border-b border-b-neutral-200  dark:border-b-slate-700 w-screen">
+        h-16 text-slate-900 border-b border-b-neutral-200  w-screen">
         <a class="navbar-brand" href="{{ route('home') }}">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-7 hover:fill-sky-500 hover:cursor-pointer">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-9 fill-sky-500 hover:fill-sky-300 hover:cursor-pointer">
                 <path d="M2.25 2.25a.75.75 0 0 0 0 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 0 0-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 0 0 0-1.5H5.378A2.25 2.25 0 0 1 7.5 15h11.218a.75.75 0 0 0 .674-.421 60.358 60.358 0 0 0 2.96-7.228.75.75 0 0 0-.525-.965A60.864 60.864 0 0 0 5.68 4.509l-.232-.867A1.875 1.875 0 0 0 3.636 2.25H2.25ZM3.75 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0ZM16.5 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Z" />
             </svg>
         </a>
@@ -11,18 +11,9 @@
         </button>
         <div class="flex items-center md:justify-between gap-x-4  md:gap-x-0 flex-row-reverse md:flex-row w-available w-moz-available">
             <div class="md:mx-auto ">
-                <ul class="flex items-center justify-center gap-x-3 font-medium *:hidden *:md:flex">
-                    <li>
-                        <a class="flex items-center gap-x-1 hover:text-sky-500 hover:cursor-pointer" href="{{ route('home')}}">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-5">
-                                <path fill-rule="evenodd" d="M9.293 2.293a1 1 0 0 1 1.414 0l7 7A1 1 0 0 1 17 11h-1v6a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-3a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-6H3a1 1 0 0 1-.707-1.707l7-7Z" clip-rule="evenodd" />
-                            </svg>
-                            Home
-                        </a>
-                    </li>
-                    <li>
+                <ul class="flex items-center justify-center gap-x-3 font-semibold *:hidden *:md:flex">
                         <a class="flex items-center gap-x-1 hover:text-sky-500 hover:cursor-pointer" href="{{ route('categorias')}}">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-5">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-5 fill-slate-400">
                                 <path fill-rule="evenodd" d="M4.25 2A2.25 2.25 0 0 0 2 4.25v11.5A2.25 2.25 0 0 0 4.25 18h11.5A2.25 2.25 0 0 0 18 15.75V4.25A2.25 2.25 0 0 0 15.75 2H4.25ZM15 5.75a.75.75 0 0 0-1.5 0v8.5a.75.75 0 0 0 1.5 0v-8.5Zm-8.5 6a.75.75 0 0 0-1.5 0v2.5a.75.75 0 0 0 1.5 0v-2.5ZM8.584 9a.75.75 0 0 1 .75.75v4.5a.75.75 0 0 1-1.5 0v-4.5a.75.75 0 0 1 .75-.75Zm3.58-1.25a.75.75 0 0 0-1.5 0v6.5a.75.75 0 0 0 1.5 0v-6.5Z" clip-rule="evenodd" />
                             </svg>
                             Categorias
@@ -30,7 +21,7 @@
                     </li>
                     <li>
                         <a class="flex items-center gap-x-1 hover:text-sky-500 hover:cursor-pointer" href="{{ route('produtos')}}">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-5">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-5 fill-slate-400">
                                 <path fill-rule="evenodd" d="M6 5v1H4.667a1.75 1.75 0 0 0-1.743 1.598l-.826 9.5A1.75 1.75 0 0 0 3.84 19H16.16a1.75 1.75 0 0 0 1.743-1.902l-.826-9.5A1.75 1.75 0 0 0 15.333 6H14V5a4 4 0 0 0-8 0Zm4-2.5A2.5 2.5 0 0 0 7.5 5v1h5V5A2.5 2.5 0 0 0 10 2.5ZM7.5 10a2.5 2.5 0 0 0 5 0V8.75a.75.75 0 0 1 1.5 0V10a4 4 0 0 1-8 0V8.75a.75.75 0 0 1 1.5 0V10Z" clip-rule="evenodd" />
                             </svg>
                             Produtos
