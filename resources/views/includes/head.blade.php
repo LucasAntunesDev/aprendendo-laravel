091b027f45b04a32ce41f8bd4bb0866dc02b3d71
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2024-01-28 09:12:25 -0300

    add: blade do head

---
+++ resources/views/includes/head.blade.php
@@ -0,0 +1,7 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema de produtos</title>
+    <link rel="icon" type="image/png" href="./favicon.svg">
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,17 +1,11 @@
 <!DOCTYPE html 5>
 <html lang="pt-BR">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sistema de produtos</title>
-    <link rel="icon" type="image/png" href="./favicon.svg">
-    <script src="https://cdn.tailwindcss.com"></script>
-</head>
+@include('includes.head')
 
-<body class="scroll-smooth bg-zinc-50 dark:bg-slate-900 text-slate-950 dark:text-zinc-50">
+<body class="scroll-smooth bg-zinc-50 text-slate-950">
     @include('includes.menu')
-    <!-- <main class="w-screen flex justify-center items-center"> -->
+    
     @yield('content')
     @include('includes.footer')
 </body>
